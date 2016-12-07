e38336c011d7f7de7215c807b6e1fba3b8a79db2
Ulric Wilfred <shaman.sir@gmail.com>
2016-12-07 01:11:22 +0100

    update scripts and configuration files to Elm 0.18

--- elm-scripts.js
+++ elm-scripts.js
@@ -2,10 +2,12 @@ const writeFile = require('fs').writeFile
 var path = require('path')
 var exec = require('child_process').exec
 
+const rootDir = process.cwd()
+const rimRafExecutable = path.join(rootDir, 'node_modules/.bin/rimraf')
 process.chdir('lib/compilers/elm-compiler/temp')
 var elmPackageJson = require(path.join(process.cwd(), 'elm-package-template.js'))
 
-exec('rimraf elm-stuff elm-package.json', function(err, stdout) {
+exec(rimRafExecutable + ' elm-stuff elm-package.json', function(err, stdout) {
   if(err) {
     console.log('error trying to delete elm-stuff', err.toString())
   } else {
@@ -16,6 +18,7 @@ exec('rimraf elm-stuff elm-package.json', function(err, stdout) {
         exec('elm-package install -y', function(err) {
           if(err) {
             console.log('Error installing elm packages')
+            console.error(err)
           }
         })
       }
